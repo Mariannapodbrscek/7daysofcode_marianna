5a0528400962f387b90134acc79a3dd1a94bdec0
Mariannapodbrscek <138744837+Mariannapodbrscek@users.noreply.github.com>
2025-02-26 07:26:42 -0600

    Update day_1_alternative_response.js

--- day_1_alternative_response.js
+++ day_1_alternative_response.js
@@ -1,35 +1,24 @@
-let numeroUn = 1;
-let stringUn = '1';
+let numeroUno = 1
+let stringUno = '1'
+let numeroTreinta = 30
+let stringTreinta = '30'
+let numeroDiez = 10
+let stringDiez = '10'
 
-let numeroTreinta = 30;
-let stringTreinta = '30';
-
-let numeroDiez = 10;
-let stringDiez = '10';
-
-// Comparación entre numeroUn y stringUn
-if (numeroUn === stringUn) {  // Usando === para verificar valor y tipo idénticos
-  console.log('Las variables numeroUn y stringUn son idénticas, tienen el mismo valor y el mismo tipo');
-} else if (numeroUn == stringUn) {  // Usando == para comparar solo los valores
-  console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes');
+if (numeroUno == stringUno) {
+  console.log('Las variables numeroUno y stringUno tienen el mismo valor, pero tipos diferentes')
 } else {
-  console.log('Las variables numeroUn y stringUn no tienen el mismo valor');
+  console.log('Las variables numeroUno y stringUno no tienen el mismo valor')
 }
 
-// Comparación entre numeroTreinta y stringTreinta
-if (numeroTreinta === stringTreinta) {  // Usando === para comparar valor y tipo
-  console.log('Las variables numeroTreinta y stringTreinta son idénticas, tienen el mismo valor y el mismo tipo');
-} else if (numeroTreinta == stringTreinta) {  // Usando == para comparar solo los valores
-  console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor, pero tipos diferentes');
+if (numeroTreinta === stringTreinta) {
+  console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo')
 } else {
-  console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo valor');
+  console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo')
 }
 
-// Comparación entre numeroDiez y stringDiez
-if (numeroDiez === stringDiez) {  // Usando === para comparar valor y tipo
-  console.log('Las variables numeroDiez y stringDiez son idénticas, tienen el mismo valor y el mismo tipo');
-} else if (numeroDiez == stringDiez) {  // Usando == para comparar solo los valores
-  console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes');
+if (numeroDiez == stringDiez) {
+  console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes')
 } else {
-  console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor');
+  console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
 }
